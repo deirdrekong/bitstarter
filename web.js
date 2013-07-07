739ad2666e3b02f3a26f454b2f05c630d47f17ab
Deirdre Kong <deirdre.y.kong@gmail.com>
2013-07-07 23:36:40 +0000

    read text from index.html

--- web.js
+++ web.js
@@ -3,7 +3,10 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var fs = require('fs');
+  var content=fs.readFileSync("index.html");
+  var buffer = new Buffer(content, "utf-8");
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
